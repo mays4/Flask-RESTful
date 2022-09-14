fb75fc16b840082df860fb29a13a16e319197291
mays4 <maysalani84@yahoo.com>
2022-09-13 20:49:55 -0400

    fixed not importing db when running the app

--- app.py
+++ app.py
@@ -15,9 +15,7 @@ app.secret_key='Noor'
 api = Api(app,errors=Flask.errorhandler)
 
 
-@app.before_first_request
-def create_tables():
-  db.create_all()
+
 jwt = JWT(app,authenticate,identity) 
 #/auth
 
